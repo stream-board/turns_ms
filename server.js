b3a2ec2c17bd9e17614f688869f70bc0d1e0d0ae
Laura Cárdenas <laura.cardenas@axiacore.com>
2018-03-11 11:34:35 -0500

    :sparkles: register socket id on redis and send confirmation to admin for board's permissions

--- server.js
+++ server.js
@@ -1,6 +1,57 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var port = process.env.PORT || 8000;
+
+//In-memory DB
 var redis = require('redis');
 var redisClient = redis.createClient(6379, 'redis');
 
+var io = require('socket.io')(http);
+
+http.listen(port, function() {
+    console.log('server started. listening on :' + port);
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('user connected');
+
+    socket.on('register id', function(userNick, userId) {
+        redisClient.set(userNick, userId, function () {
+            console.log(userNick + 'in redis');
+        });
+    });
+
+    socket.on('set admin id', function(userId) {
+        redisClient.set('admin', socket.id, function () {
+            console.log('admin id setted');
+        });
+    });
+
+    socket.on('ask for board', function(userId, userNick) {
+        console.log('user ' + userNick + ' ask');
+
+        redisClient.get('admin', function(err, socketId) {
+            console.log(socketId);
+            socket.broadcast.to(socketId).emit(
+                'ask for board',
+                userId,
+                userNick
+            );
+        });
+    });
+
+    socket.on('answer for board', function(adminRes, userId, userNick) {
+
+    });
+
+});
+
+
+//TEST: Redis connection
 redisClient.on('ready', function() {
     console.log('Redis is ready');
 });
